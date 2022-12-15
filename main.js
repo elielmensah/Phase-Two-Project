d76003976fa6cb8b6ffd1dd85751eabcbcc91a23
Eliel Mensah <mensaheliel93@gmail.com>
2022-12-15 12:35:26 +0000

    update

--- main.js
+++ main.js
@@ -9,10 +9,39 @@ $(document).ready(function() {
     $('.filter-text').toggleClass('light');
     $('.location-text').toggleClass('light');
     $('.fulltime-text').toggleClass('light');
-    $('.checkbox').toggleClass('light');
+
+    // Fulltime checkbox
+        $('.checkbox').toggleClass('blue');
 
     // Job content and description section
     $('.row').toggleClass('dark');
     $('.job-title').toggleClass('light');
    });
+
+//    Hoover state on elements starts here
+    $('.search').hover(function () {
+        $(this).css('background-color', '#939BF4');
+    }, function() {
+        $(this).css('background-color', '#5964e0');
+    });
+
+    $('button.load-more').hover(function() {
+        $(this).css('background-color', '#939BF4');
+    }, function() {
+        $(this).css('background-color', '#5964e0');
+    })
+
+    $('.job-title').toggleClass('dark');
+    // Hover state on elements ends here
+
+    $('.checkbox').click(function() {
+        $('.checkbox svg').show();
+    });
+
+    // Filter section for filtering job content by title,company, expertise or location starts here
+
+    // $('.filter-by-title_company_expertise').on('click', function(event) {
+    //     $(currentEventTarget).
+    // });
+    
 });
